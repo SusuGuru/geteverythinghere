35557ae46c84ad973ec07495b944a5d84f6b99d8
SusuGuru <sorayabrayka@gmail.com>
2025-10-17 14:17:14 +0000

    add store page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,22 +4,38 @@ import "../styles.css";
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
+    <header className="navbar">
       <div className="nav-logo">
-        <Link to="/">TechStore</Link>
+        <Link to="/">GetEverythingHere</Link>
       </div>
-
-      <ul className="nav-links">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/store">Store</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/product-details">Products</Link> {/* Will navigate once ProductDetails page exists */}
+          </li>
+          <li>
+            <Link to="/#about-us">About</Link>
+          </li>
+          <li>
+            <a
+              href="mailto:sbrayka19@gmail.com"
+              onClick={(e) => {
+                const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+                if (isMobile) {
+                  window.location.href =
+                    "https://wa.me/233547149360?text=Hello!%20I%20would%20like%20to%20inquire.";
+                  e.preventDefault();
+                }
+              }}
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
